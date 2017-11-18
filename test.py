569dbf349ccd2c2d43f3e900c929adbfcb465120
YiChinHsiao <yichinhsiao@Yi-Chins-MacBook.local>
2017-11-17 23:27:08 -0800

    add descriptions in test.py

--- test.py
+++ test.py
@@ -1,9 +1,30 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, request
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+application = Flask(__name__)
 
-if __name__ == "__main__":
-    app.run()
+@application.route('/')
+def test():
+	
+	money = request.args.get('money')
+	return money
+
+if __name__ == '__main__':
+    application.run(host='0.0.0.0',debug = True)
+
+# input  1. money   2. strategy (http://0.0.0.0:5000/?money=3000&strategy=1,2)
+
+# return error
+# money is not integer, money < 5000
+# strategy size > 2
+
+# return for each stock (JSON)
+# stock name, company name, 5 days end price, 5 days date, quantity, left money
+
+#API
+#1. getCompanyName()
+#2. get5DaysDateAndEndPrice()
+#3. runS1()
+#4. runS2()
+#5. runS3()
+#6. runS4()
+#7. runS5()
